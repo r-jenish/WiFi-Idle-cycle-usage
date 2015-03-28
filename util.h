1ecb315c372ac22bc7e4d8d722d059f9ae8abafa
Rakholiya Jenish <rakholiyajenish.07@gmail.com>
2015-03-28 13:11:09 +0530

    Add prototype of prompt

--- util.h
+++ util.h
@@ -5,6 +5,7 @@
 bool checkintput (std::string);
 void fatalerror(std::string);
 void warning(std::string);
+void prompt(std::string);
 void ack_message(std::string);
 void display_message_related_to_packet(std::string);
 void display_end_message();
