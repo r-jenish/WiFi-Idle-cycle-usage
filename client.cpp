aa067bd4a0d59ca78cd1a3eb2d1fdc4b33eb7668
Rakholiya Jenish <rakholiyajenish.07@gmail.com>
2015-03-28 22:00:15 +0530

    add send packet function

--- client.cpp
+++ client.cpp
@@ -2,19 +2,117 @@
 #include <iostream>
 #include <string>
 #include <pcap.h>
-
 #include "client.h"
+#include "infixtopostfix.h"
+#include "packetheader.h"
 #include "util.h"
 
 using namespace std;
 
-void client () {
+#define max_number_of_operands 7007
+
+u_int16_t number_of_operands;
+int32_t operands[max_number_of_operands];
+u_int8_t operators[max_number_of_operands-1];                                   // Defined by the MATH_OPERATOR_* constants
+u_int8_t number_of_operators_after_operand[max_number_of_operands];             // The positions of the operators is as required for Reverse Polish Notation.
+int32_t answer = 0;
+const u_int16_t end_packet_magic_number = 21845;                                // Must be set to 21845
+
+void fill_array (string postfix) {
+	u_int16_t operands_iterator = 0;
+	u_int16_t operator_iterator = 0;
+	u_int16_t nooao_iterator = 0;                                               //number_of_operators_after_operand_iterator
+	string num;
+	for ( int string_iterator = 0; string_iterator < postfix.length(); string_iterator++ ) {
+		if ( postfix[string_iterator] == ' ' ) continue;
+		if ( postfix[string_iterator] >= '0' && postfix[string_iterator] <= '9' ) {
+			num = "";
+			num += postfix[string_iterator];
+			while ( postfix[string_iterator+1] != ' ' ) {
+				string_iterator++;
+				num += postfix[string_iterator];
+			}
+			operands[operands_iterator] = atoi(num.c_str());
+			nooao_iterator = operands_iterator;
+			number_of_operators_after_operand[nooao_iterator] = 0;
+			operands_iterator++;
+		}
+		switch ( postfix[string_iterator] ) {
+			case '+':
+				operators[operator_iterator] = MATH_OPERATOR_PLUS;
+				operator_iterator++;
+				number_of_operators_after_operand[nooao_iterator]++;
+				break;
+			case '-':
+				operators[operator_iterator] = MATH_OPERATOR_MINUS;
+				operator_iterator++;
+				number_of_operators_after_operand[nooao_iterator]++;
+				break;
+			case '*':
+				operators[operator_iterator] = MATH_OPERATOR_MULTIPLY;
+				operator_iterator++;
+				number_of_operators_after_operand[nooao_iterator]++;
+				break;
+			case '/':
+				operators[operator_iterator] = MATH_OPERATOR_DIVIDE;
+				operator_iterator++;
+				number_of_operators_after_operand[nooao_iterator]++;
+				break;
+			case '%':
+				operators[operator_iterator] = MATH_OPERATOR_MODULO;
+				operator_iterator++;
+				number_of_operators_after_operand[nooao_iterator]++;
+				break;
+			case '&':
+				operators[operator_iterator] = MATH_OPERATOR_BITWISE_AND;
+				operator_iterator++;
+				number_of_operators_after_operand[nooao_iterator]++;
+				break;
+			case '|':
+				operators[operator_iterator] = MATH_OPERATOR_BITWISE_OR;
+				operator_iterator++;
+				number_of_operators_after_operand[nooao_iterator]++;
+				break;
+			case '^':
+				operators[operator_iterator] = MATH_OPERATOR_BITWISE_XOR;
+				operator_iterator++;
+				number_of_operators_after_operand[nooao_iterator]++;
+				break;
+		}
+	}
+	number_of_operands = operands_iterator;
+}
+
+void send_packet( pcap *handle, MathPacketHeader header, u_int8_t type ) {
+	if ( type == MATH_TYPE_REQUEST ) {
+		//SEND REQUEST TYPE PACKET
+		header.type_of_packet = MATH_TYPE_REQUEST;
+
+	} else if ( type == MATH_TYPE_ACK_ANSWER ) {
+		//SEND ACK ANSWER
+		header.type_of_packet = MATH_TYPE_ACK_ANSWER;
+		send_ack_packet(handle,header);
+	}
+}
+
+void client ( pcap_t *handle ) {
 	string infix;
+	string postfix;
 	prompt("Enter the expression in infix notation: ");
 	cin >> infix;
 	if ( !checkinput(infix) ) {
 		fatalerror("Incorrect input format");
 	}
-	convert_to_postfix(infix);						//Convert infix to postfix
+	postfix = infix_to_postfix(infix);
+	fill_array(postfix);
 	
+	MathPacketHeader header;
+	header.magic_number = 9770010;
+	header.user_id_of_requester = get_user_id_of_requester();
+	header.user_id_of_sender = 0;
+	header.request_id = generate_request_id();
+	header.number_of_operands = number_of_operands;
+	//send packet until ack or ans is received
+	//display answer
+	//send ans ack
 }
--- client.h
+++ client.h
@@ -1,8 +1,11 @@
 #ifndef _CLIENT_H_
 #define _CLIENT_H_
 
+#include <string>
 #include <pcap.h>
 
+void fill_array(std::string);
+void send_packet(pcap*, MathPacketHeader, u_int8_t);
 void client(pcap_t*);
 
 #endif
