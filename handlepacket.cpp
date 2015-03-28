602cb4a6872306ffe888d56c5866013e43bf5c06
Rakholiya Jenish <rakholiyajenish.07@gmail.com>
2015-03-28 22:08:21 +0530

    Two different functions for sending packet with data and acknowledgement packet

--- handlepacket.cpp
+++ handlepacket.cpp
@@ -2,8 +2,9 @@
 #include <string>
 #include <cstdlib>
 #include "packetheader.h"
+#include "handlepacket.h"
 
-void send_packet (pcap_t *handle, MathPacketHeader header ,u_int8_t buffer[], int length) {
+void send_packet_with_data (pcap_t *handle, MathPacketHeader header ,u_int8_t buffer[], int length) {
 	u_int8_t sendbuffer[length+50];
 	int packetsize = 0;
 	if ( pcap_datalink(handle) == DLT_PRISM_HEADER ) {
@@ -18,6 +19,62 @@ void send_packet (pcap_t *handle, MathPacketHeader header ,u_int8_t buffer[], in
 	pcap_inject(handle,sendbuffer,packetsize);
 }
 
-void get_MathPacketHeader () {
+void send_ack_packet (pcap_t *handle, MathPacketHeader header ) {
+	u_int8_t sendbuffer[50];
+	int packetsize = 0;
+	if ( pcap_datalink(handle) == DLT_PRISM_HEADER ) {
+		//add prism header to the starting of the packet and update packetsize
+	} else if ( pcap_datalink(handle) == DLT_IEEE802_11_RADIO ) {
+		//add radiotap header to the starting of the packet and update packetsize
+	}
+	memcpy(sendbuffer+packetsize,&header,19);
+	packetsize += 19;
+	pcap_inject(handle,sendbuffer,packetsize);
+}
 
+void get_packetinfo (pcap_t *handle, pcap_pkthdr *hdr, u_char packet, MathPacketHeader *header, u_int8_t buffer[]) {
+	int length = 0;
+	if ( pcap_datalink(handle) == DLT_PRISM_HEADER ) {
+		//length += something 
+	} else if ( pcap_datalink(handle) == DLT_IEEE802_11_RADIO ) {
+		//length += something
+	}
+	memcpy(header,packet+length,19);
+	length+=19
+	memcpy(buffer,packet+length,/*check it*/);
+}
+
+bool is_math_type_request ( MathPacketHeader header ) {
+	if ( header.type_of_packet == MATH_TYPE_REQUEST ) {
+		return true;
+	}
+	reutrn false;
+}
+
+bool is_math_type_ack_request ( MathPacketHeader header ) {
+	if ( header.type_of_packet == MATH_TYPE_ACK_REQUEST ) {
+		return true;
+	}
+	reutrn false;
+}
+
+bool is_math_type_send_answer ( MathPacketHeader header ) {
+	if ( header.type_of_packet == MATH_TYPE_SEND_ANSWER ) {
+		return true;
+	}
+	reutrn false;
+}
+
+bool is_math_type_ack_answer ( MathPacketHeader header ) {
+	if ( header.type_of_packet == MATH_TYPE_ACK_ANSWER ) {
+		return true;
+	}
+	reutrn false;
+}
+
+bool is_request_id_same ( MathPacketHeader header, u_int32_t request_id ) {
+	if ( header.request_id == request_id ) {
+		return true;
+	}
+	return false;
 }
--- handlepacket.h
+++ handlepacket.h
@@ -2,8 +2,15 @@
 #define _HANDLEPACKET_H_
 
 #include <pcap.h>
+#include "packetheader.h"
 
-void send_packet(pcap_t*, MathPacketHeader, u_int8_t, int);
-
+void send_packet_with_data(pcap_t*, MathPacketHeader, u_int8_t, int);
+void send_ack_packet(pcap_t*, MathPacketHeader);
+void get_packetinfo (pcap_t*, pcap_pkthdr*, u_char , MathPacketHeader*, u_int8_t);
+bool is_math_type_request(MathPacketHeader);
+bool is_math_type_ack_request(MathPacketHeader);
+bool is_math_type_send_answer(MathPacketHeader);
+bool is_math_type_ack_answer(MathPacketHeader);
+bool is_request_id_same(MathPacketHeader, u_int32_t);
 
 #endif
