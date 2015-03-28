b95c4b8e34f141952dd7d6279888cbcd5fcbc528
Rakholiya Jenish <rakholiyajenish.07@gmail.com>
2015-03-28 15:11:21 +0530

    changed query type and add pcap device

--- main.cpp
+++ main.cpp
@@ -1,20 +1,32 @@
 #include <iostream>
 #include <string>
 #include "util.h"
+#include "client.h"
 
 using namespace std;
 
-int main () {
+int main (int argc, char *argv[]) {
 	display_start_message();
-	prompt("Enter which side of the program you want to run: (1) Server (2) Client");
-	string query;
-	cin >> query;
-	if ( query == '1' ) {
-		server();
-	} else if ( query == '2' ) {
-		client();
+	if ( argc == 2 ) {
+		char *dev = argv[1];
+		pcap_t *handle;
+		char ebuf[500];
+		handle = pcap_open_live(dev,500,1,1000,ebuf);
+		if ( !handle ) {
+			fatalerror(ebuf);
+		}
+		prompt("Enter which side of the program you want to run: (1) Server (2) Client");
+		int query;
+		cin >> query;
+		if ( query == 1 ) {
+			server(handle);
+		} else if ( query == 2 ) {
+			client(handle);
+		} else {
+			fatalerror("Un-recognized input. Expected either 1 or 2.");
+		}
 	} else {
-		fatalerror("Un-recognized input. Accepted either 1 or 2.");
+		fatalerror("Usage:\n\t./wifi-math interface");
 	}
 	display_end_message();
 	return 0;
