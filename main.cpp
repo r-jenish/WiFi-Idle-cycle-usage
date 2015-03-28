de21434991fffec3f25309d49f73663fbfbf027e
Rakholiya Jenish <rakholiyajenish.07@gmail.com>
2015-03-28 12:48:13 +0530

    Initial main function commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include "util.h"
+
+using namespace std;
+
+int main () {
+	display_start_message();
+	prompt("Enter which side of the program you want to run:\n1. Server\n2. Client");
+	string query;
+	cin >> query;
+	if ( query == '1' ) {
+		server();
+	} else if ( query == '2' ) {
+		client();
+	} else {
+		fatalerror("Un-recognized input. Accepted either 1 or 2.");
+	}
+	display_end_message();
+	return 0;
+}
