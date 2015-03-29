cc3e6de9e8e4e739d15bf85daa553808696c9650
Rakholiya Jenish <rakholiyajenish.07@gmail.com>
2015-03-30 05:06:16 +0530

    removed unnecessary declaration

--- client.h
+++ client.h
@@ -1,12 +1,8 @@
 #ifndef _CLIENT_H_
 #define _CLIENT_H_
 
-#include <string>
 #include <pcap.h>
-#include "packetheader.h"
 
-void fill_array(std::string);
-void send_packet(pcap_t*, MathPacketHeader);
 void client(pcap_t*);
 
 #endif
