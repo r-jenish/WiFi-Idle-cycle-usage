4efb224e16df22dc259518131bb09b456d5de39a
Rakholiya Jenish <rakholiyajenish.07@gmail.com>
2015-03-31 00:39:45 +0530

    add new functions and corrected older ones

--- handlepacket.cpp
+++ handlepacket.cpp
@@ -5,14 +5,11 @@
 #include "packetheader.h"
 #include "handlepacket.h"
 
+
 void send_packet_with_data (pcap_t *handle, MathPacketHeader header ,u_int8_t buffer[], int length) {
-	u_int8_t sendbuffer[length+50];
+	u_int8_t sendbuffer[1000];
 	int packetsize = 0;
-	if ( pcap_datalink(handle) == DLT_PRISM_HEADER ) {
-		//add prism header to the starting of the packet and update packetsize
-	} else if ( pcap_datalink(handle) == DLT_IEEE802_11_RADIO ) {
-		//add radiotap header to the starting of the packet and update packetsize
-	}
+	wrap_datalink(pcap_datalink(handle),sendbuffer,&packetsize);
 	memcpy(sendbuffer+packetsize,&header,19);
 	packetsize += 19;
 	memcpy(sendbuffer+packetsize,buffer,length);
@@ -20,80 +17,102 @@ void send_packet_with_data (pcap_t *handle, MathPacketHeader header ,u_int8_t bu
 	pcap_inject(handle,sendbuffer,packetsize);
 }
 
-void send_ack_packet (pcap_t *handle, MathPacketHeader header ) {
+void send_ack_packet (pcap_t *handle, MathPacketHeader header) {
 	u_int8_t sendbuffer[50];
 	int packetsize = 0;
-	if ( pcap_datalink(handle) == DLT_PRISM_HEADER ) {
-		//add prism header to the starting of the packet and update packetsize
-	} else if ( pcap_datalink(handle) == DLT_IEEE802_11_RADIO ) {
-		//add radiotap header to the starting of the packet and update packetsize
-	}
+	wrap_datalink(pcap_datalink(handle),sendbuffer,&packetsize);
 	memcpy(sendbuffer+packetsize,&header,19);
 	packetsize += 19;
 	pcap_inject(handle,sendbuffer,packetsize);
 }
 
-void get_ack_packet (pcap_t *handle, MathPacketHeader *header) {
-	u_int8_t buffer[100];
+bool get_packet (pcap_t *handle, MathPacketHeader *header, u_int8_t pktbuffer[] ) {
+	u_int8_t buffer[1000];
 	pcap_pkthdr *hdr;
-	//buffer = pcap_next(handle,hdr);
-	get_MathPacketHeader(handle,header);
+	const u_char* temp = pcap_next(handle,hdr);
+	memcpy(buffer,temp,hdr->len);
+	get_packetinfo(handle,hdr,buffer,header,pktbuffer);
+	if ( is_math_type_request(*header) || is_math_type_send_answer(*header) ) {
+		return true;
+	} else {
+		return false;
+	}
 }
 
-void get_MathPacketHeader (pcap_t *handle, MathPacketHeader *header) { // help with pointer
-	int length = 0;
-	if ( pcap_datalink(handle) == DLT_PRISM_HEADER ) {
-		//length += something 
-	} else if ( pcap_datalink(handle) == DLT_IEEE802_11_RADIO ) {
-		//length += something
+bool get_ack_packet (pcap_t *handle, MathPacketHeader *header) {
+	u_int8_t buffer[100];
+	pcap_pkthdr *hdr;
+	const u_char* temp = pcap_next(handle,hdr);
+	memcpy(buffer,temp,hdr->len);
+	get_MathPacketHeader(handle,buffer,header);
+	if ( is_math_type_ack_request(*header) || is_math_type_ack_answer(*header) ) {
+		return true;
+	} else {
+		return false;
 	}
+}
+
+void get_MathPacketHeader (pcap_t *handle, u_int8_t packet[], MathPacketHeader *header) { // help with pointer
+	int length = 0;
+	length = datalink_length(pcap_datalink(handle),packet);
 	memcpy(header,packet+length,19);
-	length+=19;
 }
 
 void get_packetinfo (pcap_t *handle, pcap_pkthdr *hdr, u_char packet[], MathPacketHeader *header, u_int8_t buffer[]) {
 	int length = 0;
-	if ( pcap_datalink(handle) == DLT_PRISM_HEADER ) {
-		//length += something 
-	} else if ( pcap_datalink(handle) == DLT_IEEE802_11_RADIO ) {
-		//length += something
-	}
+	length = datalink_length(pcap_datalink(handle),packet);
 	memcpy(header,packet+length,19);
 	length+=19;
-	memcpy(buffer,packet+length,11/*check it*/);
+	memcpy(buffer,packet+length,6*header->number_of_operands+5);
 }
 
-bool is_math_type_request ( MathPacketHeader header ) {
-	if ( header.type_of_packet == MATH_TYPE_REQUEST ) {
-		return true;
+void wrap_datalink ( int datalink, u_int8_t buffer[], int *length) {
+	if ( datalink == DLT_PRISM_HEADER ) {
+		const u_int8_t prismHeader[] = {
+			0x00, 0x00, 0x00, 0x41,
+			0x08, 0x00, 0x00, 0x00
+		};
+		memcpy(buffer,prismHeader,8);
+		*length += 8;
+	} else if ( datalink == DLT_IEEE802_11_RADIO ) {
+		const u_int8_t radiotapHeader[] = { 
+			0x00, 0x00,
+			0x08, 0x00,
+			0x00, 0x00, 0x00, 0x00 
+		};
+		memcpy(buffer,radiotapHeader,8);
+		*length += 8;
 	}
-	return false;
 }
 
-bool is_math_type_ack_request ( MathPacketHeader header ) {
-	if ( header.type_of_packet == MATH_TYPE_ACK_REQUEST ) {
-		return true;
+int datalink_length ( int datalink, u_int8_t packet[] ) {
+	int length = 0;
+	if ( datalink == DLT_PRISM_HEADER ) {
+		prism_header* hdr = (prism_header*)(packet);
+		length = hdr->msglen;
+	} else if ( datalink == DLT_IEEE802_11_RADIO ) {
+		ieee80211_radiotap_header* hdr = (ieee80211_radiotap_header*)(packet);
+		length = hdr->it_len;
 	}
-	return false;
+	return length;
 }
 
-bool is_math_type_send_answer ( MathPacketHeader header ) {
-	if ( header.type_of_packet == MATH_TYPE_SEND_ANSWER ) {
-		return true;
-	}
-	return false;
+inline bool is_math_type_request ( MathPacketHeader header ) {
+	return header.type_of_packet == MATH_TYPE_REQUEST;
 }
 
-bool is_math_type_ack_answer ( MathPacketHeader header ) {
-	if ( header.type_of_packet == MATH_TYPE_ACK_ANSWER ) {
-		return true;
-	}
-	return false;
+inline bool is_math_type_ack_request ( MathPacketHeader header ) {
+	return header.type_of_packet == MATH_TYPE_ACK_REQUEST;
 }
 
-bool is_request_id_same ( MathPacketHeader header, u_int32_t request_id ) {
-	if ( header.request_id == request_id ) {
-		return true;
-	}
-	return false;
+inline bool is_math_type_send_answer ( MathPacketHeader header ) {
+	return header.type_of_packet == MATH_TYPE_SEND_ANSWER;
+}
+
+inline bool is_math_type_ack_answer ( MathPacketHeader header ) {
+	return header.type_of_packet == MATH_TYPE_ACK_ANSWER;
+}
+
+inline bool is_request_id_same ( MathPacketHeader header, u_int32_t request_id ) {
+	return header.request_id == request_id;
 }
--- handlepacket.h
+++ handlepacket.h
@@ -4,11 +4,14 @@
 #include <pcap.h>
 #include "packetheader.h"
 
-void send_packet_with_data(pcap_t*, MathPacketHeader, u_int8_t, int);
+void send_packet_with_data(pcap_t*, MathPacketHeader, u_int8_t[], int);
 void send_ack_packet(pcap_t*, MathPacketHeader);
-void get_ack_packet(pcap_t*,MathPacketHeader*);
-void get_MathPacketHeader(pcap_t*,MathPacketHeader*);
-void get_packetinfo (pcap_t*, pcap_pkthdr*, u_char , MathPacketHeader*, u_int8_t);
+bool get_packet(pcap_t*,MathPacketHeader*,u_int8_t[]);
+bool get_ack_packet(pcap_t*,MathPacketHeader*);
+void get_MathPacketHeader(pcap_t*, u_int8_t[], MathPacketHeader*);
+void get_packetinfo (pcap_t*, pcap_pkthdr*, u_char[], MathPacketHeader*, u_int8_t[]);
+void wrap_datalink(int,u_int8_t[],int*);
+int datalink_length(int,u_int8_t[]);
 bool is_math_type_request(MathPacketHeader);
 bool is_math_type_ack_request(MathPacketHeader);
 bool is_math_type_send_answer(MathPacketHeader);
